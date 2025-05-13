d7b45e945a61a8373e1215bf2274e8829738d5dd
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-13 02:58:41 -0300

    Add files via upload

---
+++ dominio/Variaveis.java
@@ -0,0 +1,14 @@
+package dominio;
+
+public class Variaveis {
+    public String Vt;
+
+    public int Vm;
+
+    
+
+    
+
+}
+
+
---
+++ test/testeDaLinguagem.java
@@ -0,0 +1,25 @@
+package test;
+
+import dominio.Variaveis;
+
+public class testeDaLinguagem {
+    public static void main(String[] args) {
+
+
+        Variaveis Vt = new Variaveis();
+        Variaveis Vm = new Variaveis();
+
+        Vt.Vt = "Eric";
+
+        Vm.Vm = 19;
+
+         
+
+
+
+
+
+
+
+    }
+}
